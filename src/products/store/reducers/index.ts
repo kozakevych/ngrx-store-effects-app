6d8048ccaaafca97d9ae88d86a8fb04d0ea19abf
Andrii Kozakevych <andriikozakevych@gmail.com>
2020-03-23 11:41:22 +0200

    create effects

---
+++ src/products/store/reducers/index.ts
@@ -0,0 +1,41 @@
+import { ActionReducerMap, createSelector, createFeatureSelector } from '@ngrx/store';
+import * as fromPizzas from './pizzas.reducer';
+
+export interface ProductsState {
+  pizzas: fromPizzas.PizzaState;
+}
+
+export const reducers: ActionReducerMap<ProductsState> = {
+  pizzas: fromPizzas.reducer,
+};
+
+export const getProductsState = createFeatureSelector<ProductsState>(
+  'products'
+);
+
+// pizzas state
+export const getPizzaState = createSelector(
+  getProductsState,
+  (state: ProductsState) => state.pizzas
+);
+
+export const getPizzasEntities = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasEntities
+);
+
+export const getAllPizzas = createSelector(
+  getPizzasEntities,
+  (entities) => {
+    return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
+  }
+);
+
+export const getAllPizzasLoaded = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoaded
+);
+export const getAllPizzasLoading = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoading
+);
